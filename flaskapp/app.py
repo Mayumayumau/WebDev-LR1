f774145b421f206047affbbdf94bdc3cd34cb849
Maya Sidorova <mayasidorova@yahoo.com>
2022-04-10 20:01:22 +0800

    add class CollageForm()

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, BooleanField
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import FileField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired, FileAllowed
 
 app = Flask(__name__)
-# TO DO: make a random key
+
+# TODO: make a random key
+
 app.config['SECRET_KEY'] = 'some_key'
+
+# TODO: CAPTCHA
+
+class CollageForm(FlaskForm):
+    img = FileField("Upload image 1", validators=[FileRequired(),
+                                                         FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
+    shape = SelectField("Collage shape", choices=[("Vertical", "Vertical"), ("Horizontal", "Horizontal")],
+                        validators=[DataRequired()])
+    submit = SubmitField("Submit", validators=[DataRequired()])
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 
