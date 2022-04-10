6e50a4840c6f75f46d83aed2618c973219b4fcd8
Maya Sidorova <mayasidorova@yahoo.com>
2022-04-10 22:18:25 +0800

    add a form on the main page

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,9 +3,10 @@ from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import FileField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileRequired, FileAllowed
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 # TODO: make a random key
 
 app.config['SECRET_KEY'] = 'some_key'
@@ -13,7 +14,9 @@ app.config['SECRET_KEY'] = 'some_key'
 # TODO: CAPTCHA
 
 class CollageForm(FlaskForm):
-    img = FileField("Upload image 1", validators=[FileRequired(),
+    img1 = FileField("Upload image 1", validators=[FileRequired(),
+                                                         FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
+    img2 = FileField("Upload image 1", validators=[FileRequired(),
                                                          FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
     shape = SelectField("Collage shape", choices=[("Vertical", "Vertical"), ("Horizontal", "Horizontal")],
                         validators=[DataRequired()])
@@ -21,6 +24,6 @@ class CollageForm(FlaskForm):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", template_form=CollageForm())
 
 
